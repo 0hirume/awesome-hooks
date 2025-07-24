a3740f65d1580d5b1b13bb8ecb1ec041a9205046
ohirume <0uhirume@gmail.com>
2025-07-24 10:26:18 +0800

    refactor: move types

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,8 @@
 export type Predicate<T> = (previous: T | undefined, current: T) => boolean;
+
+export type EventLike<T extends Callback = Callback> =
+    | { Connect(callback: T): ConnectionLike }
+    | { connect(callback: T): ConnectionLike }
+    | { subscribe(callback: T): ConnectionLike };
+
+export type ConnectionLike = { Disconnect(): void } | { disconnect(): void } | (() => void);
