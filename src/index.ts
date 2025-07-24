821d67ba11cad94cf98e0c00d700c09bbfe0e46a
ohirume <0uhirume@gmail.com>
2025-07-24 10:44:43 +0800

    feat: add exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export * from "./use-event-listener";
 export * from "./use-latest";
+export * from "./use-motion";
 export * from "./use-scaler";
+export * from "./use-spring";
 export * from "./use-text-scaler";
 export * from "./use-viewport-size";
