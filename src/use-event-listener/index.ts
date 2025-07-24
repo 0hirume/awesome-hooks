7af16039c2ef6b7e5fd135fe7a6fb88db199d488
ohirume <0uhirume@gmail.com>
2025-07-24 10:25:34 +0800

    refactor: extract types and utilities

--- src/use-event-listener/index.ts
+++ src/use-event-listener/index.ts
@@ -1,47 +1,11 @@
 import { useEffect } from "@rbxts/react";
+import { EventLike } from "../types";
 import { useLatest } from "../use-latest";
+import { connectToEvent, disconnectFromEvent } from "../utilities/connection";
 
 interface EventListenerOptions {
     readonly once?: boolean;
 }
-
-type EventLike<T extends Callback = Callback> =
-    | { Connect(callback: T): ConnectionLike }
-    | { connect(callback: T): ConnectionLike }
-    | { subscribe(callback: T): ConnectionLike };
-
-type ConnectionLike = { Disconnect(): void } | { disconnect(): void } | (() => void);
-
-const connect = (event: EventLike, callback: Callback): ConnectionLike => {
-    if (typeIs(event, "RBXScriptSignal")) {
-        const connection = event.Connect((...args: unknown[]) => {
-            if (connection.Connected) {
-                return callback(...args);
-            }
-        });
-        return connection;
-    } else if ("Connect" in event) {
-        return event.Connect(callback);
-    } else if ("connect" in event) {
-        return event.connect(callback);
-    } else if ("subscribe" in event) {
-        return event.subscribe(callback);
-    } else {
-        throw "Event-like object does not have a supported connect method.";
-    }
-};
-
-const disconnect = (connection: ConnectionLike) => {
-    if (typeIs(connection, "function")) {
-        connection();
-    } else if (typeIs(connection, "RBXScriptConnection") || "Disconnect" in connection) {
-        connection.Disconnect();
-    } else if ("disconnect" in connection) {
-        connection.disconnect();
-    } else {
-        throw "Connection-like object does not have a supported disconnect method.";
-    }
-};
 
 export function useEventListener<T extends EventLike>(
     event?: T,
@@ -59,9 +23,9 @@ export function useEventListener<T extends EventLike>(
 
         let canDisconnect = true;
 
-        const connection = connect(event, (...args: unknown[]) => {
+        const connection = connectToEvent(event, (...args: unknown[]) => {
             if (once) {
-                disconnect(connection);
+                disconnectFromEvent(connection);
                 canDisconnect = false;
             }
             listenerRef.current?.(...args);
@@ -69,7 +33,7 @@ export function useEventListener<T extends EventLike>(
 
         return () => {
             if (canDisconnect) {
-                disconnect(connection);
+                disconnectFromEvent(connection);
             }
         };
     }, [event, once, listener !== undefined]);
